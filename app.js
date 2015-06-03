f54c34e6656c72670598165bb3940b47c9ddb26b
halochou <halo.chou@gmail.com>
2015-06-03 16:36:21 +0800

    Readonly Box

--- app.js
+++ app.js
@@ -1,50 +1,5 @@
-var THREE = require('three');
-var OrbitControls = require('three-orbit-controls')(THREE);
+var box = require('./lib/box.js');
 
-var scene = require('./lib/scene.js');
-//var camera = require('./lib/camera.js');
-
-var renderer = new THREE.WebGLRenderer({
-  canvas: document.getElementById('canvas_for_three'),
-  antialias: true
+box.init({
+  'canvas': document.getElementById('canvas_for_three')
 });
-renderer.setClearColor( 0xffffff );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( canvas_to_render.scrollWidth, canvas_to_render.scrollHeight, false);
-renderer.shadowMapEnabled = true;
-renderer.shadowMapSoft = true;
-renderer.shadowMapType = THREE.PCFSoftShadowMap;
-
-var camera = new THREE.PerspectiveCamera( 30, renderer.domElement.width / renderer.domElement.height , 1, 1000 );
-camera.position.z = 100;
-camera.position.y = 0;
-
-var controls = new OrbitControls( camera, renderer.domElement);
-controls.target = new THREE.Vector3(0,0,0);
-controls.noZoom = true;
-controls.noPan = true;
-controls.minPolarAngle = Math.PI/13*6; // radians
-controls.maxPolarAngle = Math.PI/13*7; // radians
-controls.autoRotate = false;
-controls.autoRotateSpeed = 2.0;
-controls.damping = 0.2;
-
-controls.addEventListener( 'change', render );
-
-var geometry = new THREE.BoxGeometry( 2, 2, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 20;
-
-var render = function () {
-  requestAnimationFrame( render );
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
-  renderer.render(scene, camera);
-};
-
-render();
--- bundle.js
+++ bundle.js
@@ -44,56 +44,11 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var THREE = __webpack_require__(2);
-	var OrbitControls = __webpack_require__(3)(THREE);
+	var box = __webpack_require__(1);
 
-	var scene = __webpack_require__(1);
-	//var camera = require('./lib/camera.js');
-
-	var renderer = new THREE.WebGLRenderer({
-	  canvas: document.getElementById('canvas_for_three'),
-	  antialias: true
+	box.init({
+	  'canvas': document.getElementById('canvas_for_three')
 	});
-	renderer.setClearColor( 0xffffff );
-	renderer.setPixelRatio( window.devicePixelRatio );
-	renderer.setSize( canvas_to_render.scrollWidth, canvas_to_render.scrollHeight, false);
-	renderer.shadowMapEnabled = true;
-	renderer.shadowMapSoft = true;
-	renderer.shadowMapType = THREE.PCFSoftShadowMap;
-
-	var camera = new THREE.PerspectiveCamera( 30, renderer.domElement.width / renderer.domElement.height , 1, 1000 );
-	camera.position.z = 100;
-	camera.position.y = 0;
-
-	var controls = new OrbitControls( camera, renderer.domElement);
-	controls.target = new THREE.Vector3(0,0,0);
-	controls.noZoom = true;
-	controls.noPan = true;
-	controls.minPolarAngle = Math.PI/13*6; // radians
-	controls.maxPolarAngle = Math.PI/13*7; // radians
-	controls.autoRotate = false;
-	controls.autoRotateSpeed = 2.0;
-	controls.damping = 0.2;
-
-	controls.addEventListener( 'change', render );
-
-	var geometry = new THREE.BoxGeometry( 2, 2, 1 );
-	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-	var cube = new THREE.Mesh( geometry, material );
-	scene.add( cube );
-
-	camera.position.z = 20;
-
-	var render = function () {
-	  requestAnimationFrame( render );
-
-	  cube.rotation.x += 0.01;
-	  cube.rotation.y += 0.01;
-
-	  renderer.render(scene, camera);
-	};
-
-	render();
 
 
 /***/ },
@@ -102,38 +57,99 @@
 
 	var THREE = __webpack_require__(2);
 	var OrbitControls = __webpack_require__(3)(THREE);
+	var scene, canvas, renderer, camera, controls;
+	var onWindowResize;
+	var render;
 
-	var scene = new THREE.Scene();
+	var init = function (params) {
+	  // var scene = require('./lib/scene.js');
+	  canvas = params.canvas;
 
-	//light
-	var light = new THREE.AmbientLight( 0xffffff);
-	light.name = "ambientlight";
-	scene.add(light);
+	  scene = new THREE.Scene();
 
-	var directionalLight = new THREE.DirectionalLight( 0xffffff );
-	directionalLight.position.set( 0, 0, 1 ).normalize();
-	scene.add( directionalLight );
+	  //light
+	  var light = new THREE.AmbientLight( 0xffffff);
+	  light.name = "ambientlight";
+	  scene.add(light);
 
-	var directionalLight = new THREE.DirectionalLight( 0xffffff );
-	directionalLight.position.set( 0, 0, -1 ).normalize();
-	scene.add( directionalLight );
+	  var directionalLight = new THREE.DirectionalLight( 0xffffff );
+	  directionalLight.position.set( 0, 0, 1 ).normalize();
+	  scene.add( directionalLight );
 
-	var line_material = new THREE.LineBasicMaterial( { color: 0x999999 } ),
-	geometry = new THREE.Geometry(),
-	floor = -20, step = 10;
+	  var directionalLight = new THREE.DirectionalLight( 0xffffff );
+	  directionalLight.position.set( 0, 0, -1 ).normalize();
+	  scene.add( directionalLight );
 
-	for ( var i = 0; i <= 20; i ++ ) {
-	  geometry.vertices.push( new THREE.Vector3( - 100, floor, i * step - 100 ) );
-	  geometry.vertices.push( new THREE.Vector3(   100, floor, i * step - 100 ) );
+	  var line_material = new THREE.LineBasicMaterial( { color: 0x999999 } ),
+	  geometry = new THREE.Geometry(),
+	  floor = -20, step = 10;
 
-	  geometry.vertices.push( new THREE.Vector3( i * step - 100, floor, -100 ) );
-	  geometry.vertices.push( new THREE.Vector3( i * step - 100, floor,  100 ) );
+	  for ( var i = 0; i <= 20; i ++ ) {
+	    geometry.vertices.push( new THREE.Vector3( - 100, floor, i * step - 100 ) );
+	    geometry.vertices.push( new THREE.Vector3(   100, floor, i * step - 100 ) );
+
+	    geometry.vertices.push( new THREE.Vector3( i * step - 100, floor, -100 ) );
+	    geometry.vertices.push( new THREE.Vector3( i * step - 100, floor,  100 ) );
+	  }
+
+	  var line = new THREE.Line( geometry, line_material, THREE.LinePieces );
+	  scene.add( line );
+
+	  //
+
+	  renderer = new THREE.WebGLRenderer({
+	    canvas: canvas,
+	    antialias: true
+	  });
+	  renderer.setClearColor( 0xffffff );
+	  renderer.setPixelRatio( window.devicePixelRatio );
+	  renderer.setSize( canvas.scrollWidth, canvas.scrollHeight, false);
+	  renderer.shadowMapEnabled = true;
+	  renderer.shadowMapSoft = true;
+	  renderer.shadowMapType = THREE.PCFSoftShadowMap;
+
+	  camera = new THREE.PerspectiveCamera( 30, renderer.domElement.width / renderer.domElement.height , 1, 1000 );
+	  camera.position.z = 100;
+	  camera.position.y = 0;
+
+	  controls = new OrbitControls( camera, renderer.domElement);
+	  controls.target = new THREE.Vector3(0,0,0);
+	  controls.noZoom = true;
+	  controls.noPan = true;
+	  controls.minPolarAngle = Math.PI/13*6; // radians
+	  controls.maxPolarAngle = Math.PI/13*7; // radians
+	  controls.autoRotate = false;
+	  controls.autoRotateSpeed = 2.0;
+	  controls.damping = 0.2;
+	  controls.addEventListener( 'change', render );
+
+	  window.addEventListener( 'resize', onWindowResize, false );
+
+	  var geometry = new THREE.BoxGeometry( 2, 2, 1 );
+	  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	  var cube = new THREE.Mesh( geometry, material );
+	  scene.add( cube );
+
+	  render();
 	}
 
-	var line = new THREE.Line( geometry, line_material, THREE.LinePieces );
-	scene.add( line );
+	render = function () {
+	  requestAnimationFrame( render );
+	  renderer.render(scene, camera);
+	};
 
-	module.exports = scene;
+
+	onWindowResize = function () {
+		renderer.setSize( canvas.scrollWidth, canvas.scrollHeight ,false);
+		camera.aspect = renderer.domElement.width / renderer.domElement.height;
+		camera.updateProjectionMatrix();
+
+		render();
+	}
+
+	module.exports  = {
+	  init : init
+	};
 
 
 /***/ },
